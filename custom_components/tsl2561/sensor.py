89e2fdbb7c9b8ddb7bd3ba8be01dab2993f9a1a3
John Caldwell <jp.caldwell30@gmail.com>
2024-02-03 05:49:09 -0500

    rename

--- custom_components/tsl2561/sensor.py
+++ custom_components/tsl2561/sensor.py
@@ -38,7 +38,7 @@ class Tsl2561Sensor(RestoreEntity):
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Luminosita'
+        return 'TSL2561'
 
     @property
     def state(self):
