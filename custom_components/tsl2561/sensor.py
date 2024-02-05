e00f4ba9e064c24fbd1c2166fd4a1826258085c4
John Caldwell <jp.caldwell30@gmail.com>
2024-02-04 21:58:56 -0500

    change mode of getting lux

--- custom_components/tsl2561/sensor.py
+++ custom_components/tsl2561/sensor.py
@@ -1,34 +1,28 @@
 """Platform for sensor integration."""
 from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.helpers import config_validation as cv
-from homeassistant.const import CONF_PIN_NUMBER
-import RPi.GPIO as GPIO
 import voluptuous as vol
 import time
+import board
+import busio
+import adafruit_tsl2561
 
 DOMAIN = 'tsl2561'
 
-CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({
-        vol.Required("CONF_PIN_NUMBER", default=27): cv.positive_int,
-    })
-})
-
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
-    pin_number = config.get('pin_number', 27)  # Default to 27 if not provided
-    add_entities([Tsl2561Sensor(pin_number)])
+    add_entities([Tsl2561Sensor()])
 
 
 class Tsl2561Sensor(RestoreEntity):
     """Representation of a Sensor."""
 
-    def __init__(self, pin_number):
+    def __init__(self):
         """Initialize the sensor."""
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self.tsl = adafruit_tsl2561.TSL2561(i2c)
         self._state = None
-        self._pin_number = pin_number
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
+        self._attributes = {}
 
     @property
     def name(self):
@@ -45,17 +39,41 @@ class Tsl2561Sensor(RestoreEntity):
         """Return the unit of measurement."""
         return "lux"
 
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        return self._attributes
+
     def update(self):
         """Fetch new state data for the sensor."""
-        self._state = self._lightcheck()
+        self._state, self._attributes = self._lightcheck()
 
     def _lightcheck(self):
-        LDRCount = 0 # Sets the count to 0
-        GPIO.setup(self._pin_number, GPIO.OUT)
-        GPIO.output(self._pin_number, GPIO.LOW)
-        time.sleep(0.1) # Drains all charge from the capacitor
-        GPIO.setup(self._pin_number, GPIO.IN) # Sets the pin to be input
-        # While the input pin reads ‘off’ or Low, count
-        while (GPIO.input(self._pin_number) == GPIO.LOW):
-            LDRCount += 1 # Add one to the counter
-        return LDRCount
+        # Enable the light sensor
+        self.tsl.enabled = True
+        time.sleep(1)
+
+        # Set gain 0=1x, 1=16x
+        self.tsl.gain = 0
+
+        # Set integration time (0=13.7ms, 1=101ms, 2=402ms, or 3=manual)
+        self.tsl.integration_time = 1
+
+        # Get raw (luminosity) readings individually
+        broadband = self.tsl.broadband
+        infrared = self.tsl.infrared
+
+        # Get computed lux value (tsl.lux can return None or a float)
+        lux = self.tsl.lux
+
+        # Disble the light sensor (to save power)
+        self.tsl.enabled = False
+
+        attributes = {
+            "gain": self.tsl.gain,
+            "integration_time": self.tsl.integration_time,
+            "broadband": broadband,
+            "infrared": infrared,
+        }
+        
+        return lux, attributes
